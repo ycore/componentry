0dcafe9f55a6a3b96693b06af67e83fd86d1fdd0
Johan Meyer <y-core@outlook.com>
2025-03-25 17:33:02 +0200

    Update package dist

---
+++ lib/catalyst/stacked-layout.d.ts
@@ -0,0 +1,7 @@
+import type React from 'react';
+export declare function StackedLayout({ navbar, sidebar, className, children }: React.PropsWithChildren<{
+    navbar: React.ReactNode;
+    sidebar: React.ReactNode;
+    className?: string;
+}>): import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=stacked-layout.d.ts.map
---
+++ lib/common/Page.d.ts
@@ -0,0 +1,28 @@
+import type React from 'react';
+import { type NavConfig } from './NavMenu.js';
+interface PageProps {
+    className?: string;
+    footConfig: FootConfig;
+    logoRef: string;
+    navConfig: NavConfig;
+    children: React.ReactNode;
+}
+interface FootConfig {
+    title: string;
+    from: string;
+    to: () => number;
+    scripts?: Array<{
+        props: React.HTMLProps<HTMLScriptElement>;
+    }>;
+}
+interface HeaderConfig {
+    logoRef: string;
+}
+export declare function Page({ className, footConfig, logoRef, navConfig, children }: PageProps): import("react/jsx-runtime").JSX.Element;
+export declare function Header({ logoRef }: HeaderConfig): import("react/jsx-runtime").JSX.Element;
+export declare function Script(footConfig: FootConfig): import("react/jsx-runtime").JSX.Element;
+export declare function Footer({ footConfig }: {
+    footConfig: FootConfig;
+}): import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=Page.d.ts.map
