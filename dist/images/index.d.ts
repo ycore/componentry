f8b7dc9abfe9e71faeec752ff87d6afda28fa586
Johan Meyer <y-core@outlook.com>
2025-08-03 11:29:34 +0200

    Update package building

---
+++ dist/images/index.d.ts
@@ -0,0 +1,3 @@
+export type { ImageData } from './@types/LazyImage.types';
+export { imageLoader, LazyGallery, LazyImage } from './LazyImage';
+export { createSpriteIcon, SpriteIcon } from './SpriteIcon';
---
+++ dist/impetus/index.d.ts
@@ -0,0 +1,4 @@
+export { ThemeProvider } from 'next-themes';
+export { LoadingBar } from './LoadingBar';
+export { Footer, Header } from './Page';
+export { THEME_OPTIONS, ThemeSwitch } from './ThemeSwitch';
