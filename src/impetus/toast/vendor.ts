a502aa1ccea23c6a25fbcb31fcee7dc00c66c7c8
Johan Meyer <y-core@outlook.com>
2025-08-25 16:31:13 +0200

    Add CSRF secure, add error checking and update email

--- src/impetus/toast/vendor.ts
+++ src/impetus/toast/vendor.ts
@@ -1,2 +1,11 @@
-export { getToast, setToast, unstable_toastMiddleware } from 'remix-toast/middleware';
+import type { unstable_RouterContextProvider } from 'react-router';
+import { setToast as setToastVendor } from 'remix-toast/middleware';
+
+export { getToast, unstable_toastMiddleware } from 'remix-toast/middleware';
 export { toast as notify } from 'sonner';
+
+type ToastMessage = Parameters<typeof setToastVendor> extends [unknown, infer T] ? T : never;
+
+export function setToast(context: Readonly<unstable_RouterContextProvider>, toast: ToastMessage) {
+  return setToastVendor(context as unstable_RouterContextProvider, toast);
+}
